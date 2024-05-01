20f157b3f9287eb71ca61eb68157c962e68b5ffd
Dave E <dave.esselink@gmail.com>
2024-05-01 09:45:41 -0700

    1. Added dynamic data plotting using test data.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import plotly.express as px
 
 st.title("Five Day Weather Forecast")
 place = st.text_input("City: ")
@@ -8,3 +8,14 @@ days = st.slider("Forecast days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+def get_data(days):
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temps = [10, 11, 15]
+    temps = [days * i for i in temps]
+    return dates, temps
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
